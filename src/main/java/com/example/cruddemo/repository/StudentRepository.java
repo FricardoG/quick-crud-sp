bf0c1684c26e48cd0cf7417536d6f92bc168ec2b
FricardoG <74424576+FricardoG@users.noreply.github.com>
2023-10-15 06:33:22 -0600

    CRUD for Student Object

---
+++ src/main/java/com/example/cruddemo/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.cruddemo.repository;
+
+import com.example.cruddemo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
